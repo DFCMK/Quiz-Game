3486800d98e5afdea499c253aab47ccdc2aae3f8
DFCMK <dkalnoky@gmail.com>
2023-10-24 11:57:29 +0100

    created replay button and contorl buttons

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,80 @@
-const gameBoard = document.querySelector("game-board");
+let grid = document.querySelector(".grid");
+let popup = document.querySelector(".popup");
+let playAgain = document.querySelector(".playAgain");
+let ScoreDisplay = document.querySelector(".left");
+let bottom = document.querySelector(".bottom");
+let right = document.querySelector(".right");
+let up = document.querySelector(".top");
+let width = 10;
+let currentIndex = 0;
+let appleIndex = 0;
+let currentSnake = [2, 1, 0];
+let direction = 1;
+let score = 0;
+let speed = 1;
+let intervalTime = 0;
+let interval = 0;
+
+document.addEventListener("DOMContentLoaded", function (){
+    document.addEventListener("keyup", control);
+    createBoard();
+    startGame();
+    playAgain.addEventListener("click", replay);
+})
+
+function createBoard(){
+    popup.style.display = "none";
+    for (let i = 0; i < 100; i++){
+        let div = document.createElement("div");
+        grid.appendChild(div);
+    }
+}
+
+function startGame() {
+    let squares = document.querySelectorAll(".grid div");
+    randomApple(squares);
+
+    direction = 1;
+    ScoreDisplay.innerHTML = score;
+    intervalTime = 1000;
+    currentSnake = [2, 1, 0];
+    currentIndex = 0;
+    currentSnake.forEach((index) => squares[index].classList.add("snake"));
+    interval = setInterval(moveOutcome, intervalTime);
+}
+
+function moveOutcome() {
+    let squares = document.querySelectorAll(".grid div");
+
+    if (checkForHits(squares)) {
+        alert("Game Over, You hit something!");
+        popup.style.display = "flex";
+        return clearInterval(interval);
+    }
+    else
+    {
+        moveSnake(squares);
+    }
+}
+
+function moveSnake(squares) {
+    let tail = currentSnake.pop();
+    squares[tail].classList.remove("snake");
+    currentSnake.unshift(currentSnake[0] + direction);
+    eatApple(squares, tail);
+    squares[currentSnake[0]].classList.add("snake");
+}
+
+
+
+/*const gameBoard = document.getElementById("game-board");
+document.addEventListener("keydown", handleKeyDown);
 const scoreDisplay = document.getElementById("score");
 const levelDisplay = document.getElementById("level");
 
 const snake = [{x: 10, y: 10}];
 
-let direction = "right";
+let snakeDirection = "right";
 let level = 0;
 let score = 1;
 let speed = 5;
@@ -16,6 +86,32 @@ function main() {
     setTimeout(main, 1000/ speed);
 }
 
-function update() {}
+function handleKeyDown(event) {
+    if (event.key === "ArrowUp" && snakeDirection !== "down") {
+        snakeDirection = "up";
+    }
+    else if (event.key === "ArrowDown" && snakeDirection !== "up")
+    {
+        snakeDirection = "down";
+    }
+    else if (event.key === "ArrowRight" && snakeDirection !== "left")
+    {
+        snakeDirection = "right";
+    }
+    else if (event.key === "ArrowLeft" && snakeDirection !== "right")
+    {
+        snakeDirection = "left";
+    }
+}
 
-function draw() {}
+function moveSnake(){}
+
+function draw(gameBoard) {
+    snake.forEach(segment => {
+        let snakeBody = document.createElement("div");
+        snakeBody.style.gridRowStart = segment.y;
+        snakeBody.style.gridColumnStart = segment.x;
+        snakeBody.classList.add('snake');
+        gameBoard.appendChild(snakeBody);
+    });
+}*/
